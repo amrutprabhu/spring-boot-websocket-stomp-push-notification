db0e26deb8d5b4cf33138a2051fe7bdff16fd3d1
amrutprabhu <amrutprabhu42@gmail.com>
2022-08-25 13:23:19 +0200

    Adding Websocket security

---
+++ spring-boot-websocket-push-notification/src/main/java/com/amrut/prabhu/notifications/SocketSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.amrut.prabhu.notifications;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class SocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages
+                .simpDestMatchers("/all/**")
+                    .permitAll()
+                    .simpTypeMatchers(SimpMessageType.CONNECT,
+                            SimpMessageType.HEARTBEAT,
+                            SimpMessageType.UNSUBSCRIBE,
+                            SimpMessageType.DISCONNECT,
+                            SimpMessageType.MESSAGE)
+                    .permitAll()
+//                .simpMessageDestMatchers("/all/**")
+//                .permitAll()
+                .anyMessage()
+                .authenticated();
+    }
+}
